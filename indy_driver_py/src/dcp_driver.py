5167b19bccca1d5c415308adf2ae743afaddd815
gwanwoo.kim <gwanwoo.kim@neuromeka.com>
2020-04-06 16:59:03 +0900

    ;

--- indy_driver_py/src/dcp_driver.py
+++ indy_driver_py/src/dcp_driver.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python
 
 import sys
-import math
 import json
 
 from indydcp import indydcp_client
 from indydcp import indy_program_maker
 
 import rospy
+
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Header, Bool, Float64MultiArray, Empty
+from std_msgs.msg import Header, Bool, PoseArray, Empty
 from moveit_msgs.msg import MoveGroupActionResult, DisplayRobotState
 from geometry_msgs.msg import Pose
 
-def rads2degs(rad_list):
-    degs = [math.degrees(rad) for rad in rad_list]
-    return degs
-
-def degs2rads(deg_list):
-    rads = [math.radians(deg) for deg in deg_list]
-    return rads
+import utils_transf
 
 class IndyROSConnector:
     def __init__(self, robot_ip, robot_name):
@@ -33,23 +27,38 @@ class IndyROSConnector:
         rospy.init_node('indy_driver_py')
         self.rate = rospy.Rate(50) # hz
 
+        # Publish current robot state
         self.joint_state_pub = rospy.Publisher('joint_states', JointState, queue_size=10)
-        self.task_state_pub = rospy.Publisher('/indy/task_pos', Float64MultiArray, queue_size=10)
+        self.task_state_pub = rospy.Publisher('/indy/pose', Pose, queue_size=10)
 
-        self.query_joint_state_sub = rospy.Subscriber("/indy/query_joint_states", DisplayRobotState,  self.joint_state_callback, queue_size=10)
-        self.plan_result_sub = rospy.Subscriber("/move_group/result", MoveGroupActionResult, self.plan_result_callback, queue_size=1)
+        # Subscribe desired robot state
+        ## task position
+        self.query_poses_sub = rospy.Subscriber("/indy/query_poses", PoseArray,  self.pose_callback, queue_size=10)
+        
+        ## joint position
+        self.query_joint_state_sub = rospy.Subscriber("/indy/query_joint_state", JointState,  self.joint_state_callback, queue_size=10)
+        self.joint_plan_result_sub = rospy.Subscriber("/move_group/result", MoveGroupActionResult, self.plan_result_callback, queue_size=1)
+
+        # Subscribe command
         self.indy_execute_plan_sub = rospy.Subscriber("/indy/execute_plan", Bool, self.indy_execute_plan_callback, queue_size=1)
         self.stop_sub = rospy.Subscriber("/indy/stop_robot", Bool, self.stop_robot_callback, queue_size=1)
         
-        # Initialize variable
-        self.joint_pos_list = []
+        # Misc variable
+        self.joint_state_list = []
+        self.pose_list = []
         self.is_executed = False
+        self.is_task_move = False
 
     def __del__(self):
         self.indy.disconnect()
 
     def joint_state_callback(self, msg):
-        self.joint_pos_list = [msg.state.joint_state.position]
+        self.joint_state_list = [msg.state.position]
+        self.is_task_move = False
+    
+    def pose_callback(self, msg):
+        self.pose_list = [p for p in msg.poses]
+        self.is_task_move = True
         
     def indy_execute_plan_callback(self, msg):
         if msg.data == True and self.is_executed == False:
@@ -57,8 +66,8 @@ class IndyROSConnector:
 
     def plan_result_callback(self, msg):
         # download planned path from ros moveit
-        self.joint_pos_list = []
-        self.joint_pos_list = [p.positions for p in msg.result.planned_trajectory.joint_trajectory.points]
+        self.joint_state_list = []
+        self.joint_state_list = [p.positions for p in msg.result.planned_trajectory.joint_trajectory.points]
 
     def stop_robot_callback(self, msg):
         if msg.data == True:
@@ -67,12 +76,16 @@ class IndyROSConnector:
     def move_robot(self):
         prog = indy_program_maker.JsonProgramComponent(policy=0, resume_time=2)
             
-        for pos in self.joint_pos_list:
-            prog.add_joint_move_to(rads2degs(pos), vel=1, blend=5)
+        if is_task_move:
+            for pose in self.pose_list:
+                prog.add_task_move_to(utils_transf.xyz_uvw_from_pose(pose), vel=1, blend=5)
+        else:
+            for j_pos in self.joint_state_list:
+                prog.add_joint_move_to(utils_transf.rads2degs(j_pos), vel=1, blend=5)
         
         json_string = json.dumps(prog.json_program)
         self.indy.set_and_start_json_program(json_string)
-        self.joint_pos_list = []
+        self.joint_state_list = []
 
     def publish_joint_state(self):
         joint_state_msg = JointState()
@@ -83,15 +96,14 @@ class IndyROSConnector:
         else:
             joint_state_msg.name = ['joint0', 'joint1', 'joint2', 'joint3', 'joint4', 'joint5']
 
-        joint_state_msg.position = degs2rads(self.indy.get_joint_pos())
+        joint_state_msg.position = utils_transf.degs2rads(self.indy.get_joint_pos())
         joint_state_msg.velocity = []
         joint_state_msg.effort = []
 
         self.joint_state_pub.publish(joint_state_msg)
 
-    def publish_task_pos(self):
-        task_pos_msg = Float64MultiArray()
-        task_pos_msg.data = self.indy.get_task_pos()
+    def publish_pose(self):
+        task_pos_msg = utils_transf.pose_from_xyz_uvw(self.indy.get_task_pos())
         self.task_state_pub.publish(task_pos_msg)
 
     def run(self):
@@ -101,7 +113,7 @@ class IndyROSConnector:
                 self.is_executed = False
             else:
                 self.publish_joint_state()
-                self.publish_task_pos()
+                self.publish_pose()
 
             self.rate.sleep()
 
---
+++ indy_driver_py/src/utils_transf.py
@@ -0,0 +1,47 @@
+import math
+import numpy as np
+import tf
+from geometry_msgs.msg import Pose
+
+def rads2degs(rad_list):
+    degs = [math.degrees(rad) for rad in rad_list]
+    return degs
+
+def degs2rads(deg_list):
+    rads = [math.radians(deg) for deg in deg_list]
+    return rads
+
+def xyz_uvw_from_pose(self, p):
+    xyz_uvw = [0, 0, 0, 0, 0, 0]
+    xyz_uvw[0] = p.position.x
+    xyz_uvw[1] = p.position.y
+    xyz_uvw[2] = p.position.z
+
+    q = np.array((p.orientation.x,
+                        p.orientation.y,
+                        p.orientation.z,
+                        p.orientation.w))
+
+    uvw = tf.transformations.euler_from_quaternion(q)
+
+    xyz_uvw[3] = uvw[0]
+    xyz_uvw[4] = uvw[1]
+    xyz_uvw[5] = uvw[2]
+
+    return xyz_uvw
+
+def pose_from_xyz_uvw(self, xyz_uvw):
+
+    p = Pose()
+    p.position.x = xyz_uvw[0]
+    p.position.y = xyz_uvw[1]
+    p.position.z =  xyz_uvw[2]
+
+    q = tf.transformations.quaternion_from_euler(xyz_uvw[3], xyz_uvw[4], xyz_uvw[5])
+
+    p.orientation.x = q[0]
+    p.orientation.y = q[1]
+    p.orientation.z = q[2]
+    p.orientation.w = q[3]
+
+    return p
